95a79d15f8eb3ea3884d9f157acc18b817b40933
igor.mironchik@gmail.com <igor.mironchik@gmail.com@de7b6a75-3955-4f6a-5816-2b43aac99c48>
2014-01-07 12:31:44 +0000

    Implemented painting and scrolling of Picker.

--- examples/desktop/picker/main.cpp
+++ examples/desktop/picker/main.cpp
@@ -30,14 +30,49 @@
 
 // Qt include.
 #include <QApplication>
+#include <QWidget>
+#include <QVBoxLayout>
 
 // QtMWidgets include.
 #include <QtMWidgets/Picker>
 
 
+class Widget
+	:	public QWidget
+{
+public:
+	Widget()
+	{
+		QVBoxLayout * l = new QVBoxLayout( this );
+		QtMWidgets::Picker * picker = new QtMWidgets::Picker( this );
+
+		picker->addItem( QLatin1String( "English" ) );
+		picker->addItem( QLatin1String( "Russian" ) );
+		picker->addItem( QLatin1String( "German" ) );
+		picker->addItem( QLatin1String( "Spanish" ) );
+		picker->addItem( QLatin1String( "Portuguese" ) );
+		picker->addItem( QLatin1String( "Belorussian" ) );
+		picker->addItem( QLatin1String( "Polish" ) );
+		picker->addItem( QLatin1String( "Ukrainian" ) );
+
+		QFont font = picker->font();
+		font.setBold( true );
+		font.setPointSize( 15 );
+
+		picker->setFont( font );
+
+		l->addWidget( picker );
+	}
+};
+
+
 int main( int argc, char ** argv )
 {
 	QApplication app( argc, argv );
+
+	Widget w;
+
+	w.show();
 
 	return app.exec();
 }

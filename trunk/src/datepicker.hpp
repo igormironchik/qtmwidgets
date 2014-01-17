d144d0eaf97a01029ac92e5b5b76f2eac4387a77
igor.mironchik@gmail.com <igor.mironchik@gmail.com@de7b6a75-3955-4f6a-5816-2b43aac99c48>
2014-01-17 12:16:54 +0000

    Implemented DatePicker.

--- trunk/src/datepicker.hpp
+++ trunk/src/datepicker.hpp
@@ -31,8 +31,40 @@
 #ifndef QTMWIDGETS__DATEPICKER_HPP__INCLUDED
 #define QTMWIDGETS__DATEPICKER_HPP__INCLUDED
 
+// QtMWidgets include.
+#include "datetimepicker.hpp"
+
 
 namespace QtMWidgets {
+
+//
+// DatePicker
+//
+
+/*!
+	The DatePicker class provides widget for selecting
+	date. DatePicker shows cylinders with separate
+	components of date, i.e. year, month, day.
+	By default format of the DatePicker is "yyyy MMMM dddd",
+	that mean that widgets will show 3 cylinders with year, long month name
+	and long day name with day number. \sa setFormat().
+
+	DateTimePicker allows to select date from the given
+	range. By default minimum date is September 14, 1752
+	and maximum is December 31, 7999.
+*/
+class DatePicker
+	:	public DateTimePicker
+{
+	Q_OBJECT
+	Q_PROPERTY( QDate date READ date WRITE setDate USER true )
+
+public:
+	explicit DatePicker( QWidget * parent = 0 );
+	explicit DatePicker( const QDate & date, QWidget * parent = 0 );
+
+	~DatePicker();
+};
 
 } /* namespace QtMWidgets */
 

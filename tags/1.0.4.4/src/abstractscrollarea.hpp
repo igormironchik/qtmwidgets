f573b1002bf3b684afffac2f9e7c2fb60ae86342
igor.mironchik@gmail.com <igor.mironchik@gmail.com@de7b6a75-3955-4f6a-5816-2b43aac99c48>
2014-12-21 05:18:56 +0000

    Tag 1.0.4.4

---
+++ tags/1.0.4.4/src/abstractscrollarea.hpp
@@ -0,0 +1,218 @@
+
+/*!
+	\file
+
+	\author Igor Mironchik (igor.mironchik at gmail dot com).
+
+	Copyright (c) 2014 Igor Mironchik
+
+	Permission is hereby granted, free of charge, to any person
+	obtaining a copy of this software and associated documentation
+	files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use,
+	copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following
+	conditions:
+
+	The above copyright notice and this permission notice shall be
+	included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef QTMWIDGETS__ABSTRACTSCROLLAREA_HPP__INCLUDED
+#define QTMWIDGETS__ABSTRACTSCROLLAREA_HPP__INCLUDED
+
+// Qt include.
+#include <QFrame>
+#include <QScopedPointer>
+
+
+namespace QtMWidgets {
+
+//
+// AbstractScrollArea
+//
+
+class AbstractScrollAreaPrivate;
+
+/*!
+	\brief The AbstractScrollArea widget provides a scrolling area with
+	on-demand scrolling indicators.
+
+	AbstractScrollArea is a low-level abstraction of a scrolling
+	area. The area provides a central widget called the viewport, in
+	which the contents of the area is to be scrolled (i.e, the
+	visible parts of the contents are rendered in the viewport).
+
+	It is possible to reserve a margin area around the viewport, see
+	setViewportMargins(). Subclasses of AbstractScrollArea should
+	implement margins.
+
+	When inheriting AbstractScrollArea, you need to do the
+	following:
+
+	\li Control the scrolled area size and top-left corner position
+		of shown part of scrolled area.
+	\li Draw the contents of the area in the viewport according
+		to the value of the top-left position of shown area.
+	\li Use \c{viewport->update()} to update the contents of the
+		viewport instead of \link{QWidget::update()}{update()}\endlink
+		as all painting operations take place on the viewport.
+
+	ScrollArea, which inherits AbstractScrollArea, provides smooth
+	scrolling for any QWidget (i.e., the widget is scrolled pixel by
+	pixel). You only need to subclass AbstractScrollArea if you need
+	more specialized behavior. This is, for instance, true if the
+	entire contents of the area is not suitable for being drawn on a
+	QWidget or if you do not want smooth scrolling.
+
+	\sa ScrollArea
+*/
+class AbstractScrollArea
+	:	public QFrame
+{
+	Q_OBJECT
+
+	Q_ENUMS( ScrollIndicatorPolicy )
+
+	/*!
+		\property indicatorColor
+
+		\brief Color used to paint scrolling indicators.
+
+		By default this color is QPalette::Highlight.
+	*/
+	Q_PROPERTY( QColor indicatorColor READ indicatorColor WRITE setIndicatorColor )
+	/*!
+		\property verticalScrollIndicatorPolicy
+
+		\brief Policy specified to display or not vertical scroll indicator.
+
+		By default this property is ScrollIndicatorAsNeeded.
+	*/
+	Q_PROPERTY( ScrollIndicatorPolicy verticalScrollIndicatorPolicy
+		READ verticalScrollIndicatorPolicy
+		WRITE setVerticalScrollIndicatorPolicy )
+	/*!
+		\property horizontalScrollIndicatorPolicy
+
+		\brief Policy specified to display or not horizontal scroll indicator.
+
+		By default this property is ScrollIndicatorAsNeeded.
+	*/
+	Q_PROPERTY( ScrollIndicatorPolicy horizontalScrollIndicatorPolicy
+		READ horizontalScrollIndicatorPolicy
+		WRITE setHorizontalScrollIndicatorPolicy )
+
+public:
+	/*!
+		This enums describes the variuos modes of scroll indicators.
+	*/
+	enum ScrollIndicatorPolicy {
+		/*!
+			AbstractScrollArea shows scroll indicators when content
+			is too large to fit and not otherwise. This is default.
+		*/
+		ScrollIndicatorAsNeeded = 0,
+		//! AbstractScrollArea never shows scroll indocators.
+		ScrollIndicatorAlwaysOff = 1,
+		//! AbstractScrollArea always shows scroll indicators.
+		ScrollIndicatorAlwaysOn = 2
+	}; // enum ScrollIndicatorPolicy
+
+public:
+	AbstractScrollArea( QWidget * parent = 0 );
+	virtual ~AbstractScrollArea();
+
+	//! \return Viewport widget.
+	QWidget * viewport() const;
+	//! Set viewport widget.
+	void setViewport( QWidget * widget );
+	/*!
+		This method is called by AbstractScrollArea after setViewport(\a
+		viewport) has been called. Reimplement this function in a
+		subclass of AbstractScrollArea to initialize the new \a viewport
+		before it is used.
+
+		\sa setViewport()
+	*/
+	virtual void setupViewport( QWidget * viewport );
+
+	//! \return Color used to paint scrolling indicators.
+	QColor indicatorColor() const;
+	//! Set color used to paint scrolling indicator.
+	void setIndicatorColor( const QColor & c );
+
+	//! \return Vertical scroll indicator policy.
+	ScrollIndicatorPolicy verticalScrollIndicatorPolicy() const;
+	//! Set vertical scroll indicator policy.
+	void setVerticalScrollIndicatorPolicy( ScrollIndicatorPolicy policy );
+
+	//! \return Horizontal scroll indicator policy.
+	ScrollIndicatorPolicy horizontalScrollIndicatorPolicy() const;
+	//! Set horizontal scroll indicator policy.
+	void setHorizontalScrollIndicatorPolicy( ScrollIndicatorPolicy policy );
+
+	virtual QSize minimumSizeHint() const;
+	virtual QSize sizeHint() const;
+
+protected:
+	explicit AbstractScrollArea( AbstractScrollAreaPrivate * dd,
+		QWidget * parent = 0 );
+
+	//! Set viewport margins.
+	void setViewportMargins( int left, int top, int right, int bottom );
+	//! Set viewport margins.
+	void setViewportMargins( const QMargins & margins );
+
+	/*!
+		Scroll contents. Should be reimplemented in a subclass.
+	*/
+	virtual void scrollContentsBy( int dx, int dy ) = 0;
+
+	//! \return Size of the scrolled area.
+	const QSize & scrolledAreaSize() const;
+	//! Set scrolled area size.
+	void setScrolledAreaSize( const QSize & s );
+
+	//! \return Top-left corner of the shown scrolled area.
+	const QPoint & topLeftPointShownArea() const;
+	//! Set top-left corner of the shown scrolled area.
+	void setTopLeftPointShownArea( const QPoint & p );
+
+	//! Start animation of fading scroll indicators.
+	void startScrollIndicatorsAnimation();
+
+	virtual void resizeEvent( QResizeEvent * e );
+	virtual void mousePressEvent( QMouseEvent * e );
+	virtual void mouseReleaseEvent( QMouseEvent * e );
+	virtual void mouseMoveEvent( QMouseEvent * e );
+	virtual void wheelEvent( QWheelEvent * e );
+
+protected:
+	QScopedPointer< AbstractScrollAreaPrivate > d;
+
+	friend class AbstractScrollAreaPrivate;
+
+private slots:
+	void _q_animateScrollIndicators();
+	void _q_kineticScrolling( int dx, int dy );
+	void _q_kineticScrollingAboutToStart();
+	void _q_kineticScrollingFinished();
+
+private:
+	Q_DISABLE_COPY( AbstractScrollArea )
+}; // class AbstractScrollArea
+
+} /* namespace QtMWidgets */
+
+#endif // QTMWIDGETS__ABSTRACTSCROLLAREA_HPP__INCLUDED
